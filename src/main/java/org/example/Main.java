bb7fa9953c8c6c9dcf5206ba32b7558805d2ddc0
yang.zhang <yang.zhao@oceanus-plus.com>
2022-12-02 16:53:04 +0800

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+*
+*
+* @author ryan
+* @version id: ${NAME}, v 0.1 ${DATE} ${TIME} ${USER} Exp $
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
